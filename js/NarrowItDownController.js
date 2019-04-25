73f1c7a4e52152f59c272156304c0839dddddf31
Danfeng Shao <danfeng.shao@udg.de>
2019-04-25 13:35:05 +0200

    interview

--- js/NarrowItDownController.js
+++ js/NarrowItDownController.js
@@ -25,6 +25,9 @@ function NarrowItDownController (GetJsonService){
 	var Searcher = this;
 	Searcher.name = "搜索";
 	Searcher.showthings = false;
+	Searcher.count = 0;
+	
+	
 	
 	
 	
@@ -56,12 +59,29 @@ function NarrowItDownController (GetJsonService){
 			
 			
 		}
-		
+		Searcher.count = Searcher.count +1;
 		Searcher.items = foundItems;
 		if (foundItems.length >0){
 			Searcher.showthings = true;
+			$('#search-content').css('display', 'block');
+			$('#search-content')
+			.mouseover(function(){
+				$('#search-content').css('display', 'block');
+			})
+			.mouseout(function(){
+				$('#search-content').css('display', 'none');
+			});
+			
 		}
-		else {Searcher.showthings = false;}
+		else {Searcher.showthings = false;
+			$('#search-content').css('display', 'block');
+			$('#search-content')
+			.mouseover(function(){
+				$('#search-content').css('display', 'block');
+			})
+			.mouseout(function(){
+				$('#search-content').css('display', 'none');
+			});}
 		
 		
 		
